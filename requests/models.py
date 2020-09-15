aea5b31d8c9c198bda6f2b94b356361aa1f9b5c7
Chris Tack <chris_tack@mac.com>
2020-09-15 16:54:54 -0700

    Create db model

--- requests/models.py
+++ requests/models.py
@@ -6,3 +6,8 @@ from django.db import models
 # time of request (datetime)
 # comment (text, NULL by default)
 
+class Request(models.Model):
+  # id = models.AutoField(primary_key=True)
+  requestType = models.CharField(max_length=10)
+  requestTime = models.DateTimeField('date created')
+  comment = models.CharField(None, max_length=250)
