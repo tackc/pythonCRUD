ef2437abbfae605677de144fa152cd7133fff38b
Chris Tack <chris_tack@mac.com>
2020-09-15 16:54:24 -0700

    List db requirements

--- requests/models.py
+++ requests/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+# Has a SQLite DB with a table named `requests` that has the following columns:
+# id (viz. autoincrementing int)
+# type of request (e.g. GET, POST, PUT, DELETE)
+# time of request (datetime)
+# comment (text, NULL by default)
+
