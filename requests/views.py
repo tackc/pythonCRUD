9c8f03d1b179f139074204a465a8b5fa5fe92287
Chris Tack <chris_tack@mac.com>
2020-09-16 21:58:32 -0700

    Import model

--- requests/views.py
+++ requests/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Request
 
 def index(request):
     return HttpResponse("Welcome to the requests page.")
