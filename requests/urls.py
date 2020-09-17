d9823fcc1a2e26e61d57afd0eb38558c107223d7
Chris Tack <chris_tack@mac.com>
2020-09-16 23:43:54 -0700

    Add routes

--- requests/urls.py
+++ requests/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.requests_index, name='index'),
+    path('about/', views.about, name='about'),
+    path('requests/', views.requests_index, name='index'),
+    path('requests/<int:request_id>/', views.requests_detail, name='detail'),
+    path('requests/create/', views.RequestCreate.as_view(), name='requests_create'),
 ]
