02f7d17dd34ef9e300e2c223dc9bf70802249c7f
Chris Tack <chris_tack@mac.com>
2020-09-17 23:29:30 -0700

    Add route to display request data

--- requests/urls.py
+++ requests/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('requests/', views.requests_index, name='index'),
     path('requests/<int:request_id>/', views.requests_detail, name='detail'),
     path('requests/create/', views.RequestCreate.as_view(), name='requests_create'),
+    path('data/', views.data, name='data'),
 ]
