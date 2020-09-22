407000357dd73c426182a9d73155c2831a89bfda
Chris Tack <chris_tack@mac.com>
2020-09-21 21:31:16 -0700

    Create api routes

--- requests/urls.py
+++ requests/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('requests/', views.requests_index, name='index'),
     path('requests/<int:request_id>/', views.requests_detail, name='detail'),
     path('requests/create/', views.RequestCreate.as_view(), name='requests_create'),
+    path('api', views.requests_index, name='index'),
+    path('api/<int:request_id>', views.requests_detail, name='detail')
 ]
